fc0acebde4a1e716ed2051366d69ec2c4e82e059
cloudsapp <cloudsapp@126.com>
2016-10-19 17:00:29 +0800

    add screenshot

--- app/src/main/java/com/baidusoso/lessonimmersive/ImmersiveModeActivity.java
+++ app/src/main/java/com/baidusoso/lessonimmersive/ImmersiveModeActivity.java
@@ -50,6 +50,7 @@ public class ImmersiveModeActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
         int systemUiVisibility = decor.getSystemUiVisibility();
         int flags = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
                 | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
@@ -69,6 +70,7 @@ public class ImmersiveModeActivity extends AppCompatActivity {
         View decor = activity.getWindow().getDecorView();
         int systemUiVisibility = decor.getSystemUiVisibility();
         int flags = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_FULLSCREEN
